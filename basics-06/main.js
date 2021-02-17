b6f11ff145635100ea35969b256e3a9825650e27
mondragon-ai <angel@plutusx.com>
2021-02-17 16:32:07 -0700

    basics-06

---
+++ basics-06/main.js
@@ -0,0 +1,105 @@
+console_title('Built In Objects');
+// Built In Objects
+// ======================================
+var myStr = "Hello, my name is Deku"
+console.log(".lenth:", myStr.length) 
+// prints the length of the string: 22
+console.log(".toUpperCase:", myStr.toUpperCase()) 
+// prints: "HELLO, MY NAME IS DEKU"
+console.log(".toLowerCase:", myStr.toLowerCase()) 
+// prints: "hello, my name is deku"
+console.log(".chartAt:", myStr.charAt(3)) 
+// prints: "l"
+console.log(".indexOf:", myStr.indexOf("Deku")) 
+// prints: 19
+
+console_title('Object Literals');
+// Object Literals
+// ======================================
+let basic_obj = {
+    name: "angel",
+    age: 25,
+    isMale: true,
+    hobbies: ["being hungry", "sleeping"]
+}
+
+console.log("Obj Literal Dot Notaion: ", basic_obj.name); // dot notations attributes
+
+console.log("Obj Literal Brack Notation: ", basic_obj["name"]); // braket notation (variable key)
+
+console_title('Create Object');
+// Create Object
+// ======================================
+var obj = new Object({
+    hero: 'deku',
+    class: 2,
+    power: ['speed','power']
+})
+
+console.log("obj.hero:", obj.hero)
+// prints: Deku
+
+console_title('Reassign Attributes');
+// Reassign Attributes
+// ======================================
+var deku = new Object({
+    hero: 'deku',
+    class: 2,
+    powers: ['speed','power']
+})
+var flying = new Object({
+    powers: [ ...deku.powers, 'flying']
+})
+
+Object.assign(deku,flying)
+
+console.log("Obj Deku:", deku)
+// prints: {hero: 'deku', class: 2, powers: ['speed','power', 'flying']}
+
+console_title('Methods: Object.assign()');
+// Methods: Object.assign()
+// ======================================
+var deku = new Object({
+    hero: 'deku',
+    class: 2,
+    powers: ['speed','power']
+})
+var flying = new Object({
+    powers: [ ...deku.powers, 'flying']
+})
+
+const newHero = Object.assign({},deku,flying)
+
+console.log("New Hero Obj:", newHero)
+// prints: {[hero: 'deku', class: 2, powers: ['speed','power', 'flying']}
+
+console_title('Methods: Object.entries()');
+// Methods: Object.entries()
+// ======================================
+var deku = new Object({
+    hero: 'deku',
+    class: 2,
+    powers: ['speed','power']
+})
+console.log("Object.entries:", Object.entries(deku))
+// prints: {["hero": 'deku'], ["class": 2], ["powers": ['speed','power', 'flying']]}
+
+console_title('Dot Notation');
+// Dot Notation
+// ======================================
+var newDate = new Date()
+console.log(".toLocaleDateString:", newDate.toLocaleDateString());
+// prints: 5/2/2020 12:15:16 PM
+console.log(".getHours:", newDate.getHours())
+// prints: 12
+console.log(".getFullYear:", newDate.getFullYear())
+// prints: 2020
+console.log(".getFullYear:", newDate.getFullYear())
+// prints: 2020
+
+// Example Function Homwork
+// ======================================
+function console_title(title) {
+    console.log(`\n// ${title}`)
+    console.log("// ======================================")
+}
